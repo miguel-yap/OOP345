6cebdf02ea9f7dcd60adf8babd62a45f3c44c965
UuziZuZ <miguelyap9@gmail.com>
2025-10-09 21:31:43 -0400

    Completed Assignment 2

--- assignments/Asgn-02/release/characterTpl.h
+++ assignments/Asgn-02/release/characterTpl.h
@@ -6,6 +6,7 @@ Author: Miguel Yap, StNo: 168049237, Email: myap1@myseneca.ca
 -----------------------------------------------------------
 - Line 31-42 was assisted with the help of ChatGPT.
 -----------------------------------------------------------*/
+
 #ifndef SENECA_CHARACTERTPL_H
 #define SENECA_CHARACTERTPL_H
 
@@ -14,39 +15,48 @@ Author: Miguel Yap, StNo: 168049237, Email: myap1@myseneca.ca
 
 namespace seneca {
 
-template <typename T>
-class CharacterTpl : public Character {
-    int m_healthMax{};
-    T m_health{};
-public:
-    CharacterTpl(const char* name, int healthMax)
-        : Character(name), m_healthMax(healthMax), m_health{} {
-        m_health = healthMax;
-    }
+    template <typename T>
+    class CharacterTpl : public Character {
+        int m_healthMax{};
+        T m_health{};
 
-    void takeDamage(int dmg) override {
-    m_health -= dmg;
+    public:
+        CharacterTpl(const char* name, int healthMax)
+            : Character(name), m_healthMax(healthMax), m_health{} {
+            m_health = healthMax;
+        }
 
-    if (static_cast<int>(m_health) <= 0) {
-        std::cout << getName() << " has been defeated!" << std::endl;
-    } else {
-        std::cout << "    " << getName()
-                  << " took " << dmg
-                  << " damage, " << static_cast<int>(m_health)
-                  << " health remaining." << std::endl;
-    }
+        void takeDamage(int dmg) override {
+            m_health -= dmg;
 
-    int getHealth() const override { return static_cast<int>(m_health); }
-    int getHealthMax() const override { return m_healthMax; }
+            if (static_cast<int>(m_health) <= 0) {
+                std::cout << getName() << " has been defeated!" << std::endl;
+            } else {
+                std::cout << "    " << getName()
+                          << " took " << dmg
+                          << " damage, " << static_cast<int>(m_health)
+                          << " health remaining." << std::endl;
+            }
+        }
 
-    void setHealth(int health) override { m_health = health; }
+        int getHealth() const override {
+            return static_cast<int>(m_health);
+        }
 
-    void setHealthMax(int health) override {
-        m_healthMax = health;
-        m_health = health;
-    }
-};
+        int getHealthMax() const override {
+            return m_healthMax;
+        }
+
+        void setHealth(int health) override {
+            m_health = health;
+        }
+
+        void setHealthMax(int health) override {
+            m_healthMax = health;
+            m_health = health;
+        }
+    };
 
 } // namespace seneca
 
-#endif
+#endif // SENECA_CHARACTERTPL_H
